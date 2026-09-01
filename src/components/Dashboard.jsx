295f9e0ce5e429d1326e829d545b40721caef75e
uneechung-lab <unee.chung@gmail.com>
2026-09-01 12:58:05 +0900

    style: move character up by 2px (top: -27px)

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1081,7 +1081,7 @@ export default function Dashboard({
           style={{
             position: 'absolute',
             left: '48px',
-            top: '-25px',
+            top: '-27px',
             height: '110px',
             width: 'auto',
             objectFit: 'contain',
